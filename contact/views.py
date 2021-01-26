44344721100a7617aa90b3b37459474ef1a48eb6
Chris Garlick <cgarlick94@gmail.com>
2021-01-26 00:43:52 +0000

    Initial Commit

---
+++ contact/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import request
+from django.core.mail import send_mail
+from django.conf import settings
+
+
+def ContactView(request):
+    if request.method == "POST":
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message = request.POST['message']
+        msg_mail = str(message) + " " + str(message_email)
+
+        send_mail(
+            "Contacted by " + message_name,
+            msg_mail, 
+            message_email, 
+            [settings.EMAIL_HOST_USER],
+            fail_silently=False
+            )
+        return render(request, 'contact.html', {'message_name':message_name,
+                                                'message_email': message_email,
+                                                'message': message})
+    else:
+        return render(request, 'contact.html', {})
